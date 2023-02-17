131555140da789e69b90d4cf470896677e9f98e7
Daniele <danielemeno96@gmail.com>
2023-02-17 17:02:01 +0100

    fix auctionEnd if there's no bid

--- contracts/UrbEAuction.sol
+++ contracts/UrbEAuction.sol
@@ -169,19 +169,13 @@ contract UrbEAuction is ReentrancyGuard, Ownable {
             revert AuctionNotYetEnded();
         }
 
-        // Controllo se l'oggetto in vendita ha ricevuto offerte
-        if (listedItem.price == 0) {
-            // Nessuna offerta ricevuta, eliminazione del listing
-            delete s_listings[nftAddress][tokenId];
-
-            // Visualizzazione del messaggio di conferma
-            emit AuctionEnded(i_deployer, nftAddress, tokenId, 0);
-            return;
+        if (s_listings[nftAddress][tokenId].highestBidder == i_deployer) {
+            delete (s_listings[nftAddress][tokenId]);
+        } else {
+            s_proceeds[i_deployer] += listedItem.price;
+            delete (s_listings[nftAddress][tokenId]);
+            IERC721(nftAddress).safeTransferFrom(i_deployer, listedItem.highestBidder, tokenId);
         }
-
-        s_proceeds[i_deployer] += listedItem.price;
-        delete (s_listings[nftAddress][tokenId]);
-        IERC721(nftAddress).safeTransferFrom(i_deployer, listedItem.highestBidder, tokenId);
 
         emit AuctionEnded(listedItem.highestBidder, nftAddress, tokenId, listedItem.price);
     }
