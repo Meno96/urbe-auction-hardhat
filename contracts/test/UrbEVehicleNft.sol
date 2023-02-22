468898f2a7d11db189f5c5a1ab5e96cecf9f20a3
Daniele <danielemeno96@gmail.com>
2023-02-22 11:18:39 +0100

    redeploy UrbEVehicleNft

--- contracts/test/UrbEVehicleNft.sol
+++ contracts/test/UrbEVehicleNft.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract UrbEVehicleNft is ERC721URIStorage {
     // NFT Variables
