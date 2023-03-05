7bfab073cc0023edd1e308acfd196863dbca5be2
Daniele <danielemeno96@gmail.com>
2023-03-05 16:05:17 +0100

    comments and readme

--- contracts/UrbEAuction.sol
+++ contracts/UrbEAuction.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+// Defines custom errors that can be thrown by the contract
 error AlreadyListed(address nftAddress, uint256 tokenId);
 error NotListed(address nftAddress, uint256 tokenId);
 error BidNotHighEnough(address nftAddress, uint256 tokenId, uint256 price);
@@ -17,7 +18,14 @@ error NoProceeds();
 error OnlyNotOwner();
 error TimeMustBeAboveZero();
 
+/**
+ * @title UrbEAuction
+ * @notice This contract implements an auction system for ERC721 tokens.
+ * Buyers can place bids for listed items and sellers can receive payments for their tokens.
+ * The auction supports a timer and automatically transfers the token to the highest bidder after the timer has expired.
+ */
 contract UrbEAuction is ReentrancyGuard {
+    // Struct for a listing on the auction
     struct Listing {
         uint256 price;
         uint256 startTime;
@@ -29,9 +37,12 @@ contract UrbEAuction is ReentrancyGuard {
 
     address private immutable i_deployer;
 
+    // Mapping of listings to their corresponding NFTs
     mapping(address => mapping(uint256 => Listing)) private s_listings;
+    // Mapping of proceeds from sales to their respective sellers
     mapping(address => uint256) private s_proceeds;
 
+    // Event emitted when a new item is listed on the auction
     event ItemListed(
         address indexed seller,
         address indexed nftAddress,
@@ -41,8 +52,10 @@ contract UrbEAuction is ReentrancyGuard {
         uint256 startTime
     );
 
+    // Event emitted when an item is canceled from the auction
     event ItemCanceled(address indexed seller, address indexed nftAddress, uint256 indexed tokenId);
 
+    // Event emitted when a bid is placed and the highest bid is increased
     event HighestBidIncreased(
         address indexed bidder,
         address indexed nftAddress,
@@ -50,6 +63,7 @@ contract UrbEAuction is ReentrancyGuard {
         uint256 price
     );
 
+    // Event emitted when the auction is ended and the winner is determined
     event AuctionEnded(
         address indexed winner,
         address indexed nftAddress,
@@ -59,6 +73,7 @@ contract UrbEAuction is ReentrancyGuard {
         bytes auctionJson
     );
 
+    // Modifier that checks if an item is not currently listed on the auction
     modifier notListed(address nftAddress, uint256 tokenId) {
         Listing memory listing = s_listings[nftAddress][tokenId];
         if (listing.isListed) {
@@ -67,6 +82,7 @@ contract UrbEAuction is ReentrancyGuard {
         _;
     }
 
+    // Modifier that checks if an item is currently listed on the auction
     modifier isListed(address nftAddress, uint256 tokenId) {
         Listing memory listing = s_listings[nftAddress][tokenId];
         if (!listing.isListed) {
@@ -75,6 +91,7 @@ contract UrbEAuction is ReentrancyGuard {
         _;
     }
 
+    // Modifier that checks if the caller is not the deployer
     modifier onlyNotOwner() {
         if (msg.sender == i_deployer) {
             revert OnlyNotOwner();
@@ -82,6 +99,7 @@ contract UrbEAuction is ReentrancyGuard {
         _;
     }
 
+    // Modifier that checks if a given time is greater than 0
     modifier timeAboveZero(uint256 time) {
         if (time <= 0) {
             revert TimeMustBeAboveZero();
@@ -89,6 +107,7 @@ contract UrbEAuction is ReentrancyGuard {
         _;
     }
 
+    // Constructor function that sets the deployer address
     constructor() {
         i_deployer = msg.sender;
     }
@@ -96,11 +115,12 @@ contract UrbEAuction is ReentrancyGuard {
     ////////////////////
     // Main Functions //
     ////////////////////
-    /*
+    /**
      * @notice Method for listing NFT
      * @param nftAddress Address of NFT contract
      * @param tokenId Token ID of NFT
      * @param price sale price for each item
+     * @param biddingTime time of the auction
      */
     function listItem(
         address nftAddress,
@@ -134,6 +154,11 @@ contract UrbEAuction is ReentrancyGuard {
         emit ItemListed(msg.sender, nftAddress, tokenId, price, endTime, startTime);
     }
 
+    /**
+     * @notice Function for a seller to cancel a listing and remove it from the auction
+     * @param nftAddress The address of the ERC721 contract for the item being canceled
+     * @param tokenId The ID of the token being canceled
+     */
     function cancelListing(
         address nftAddress,
         uint256 tokenId
@@ -146,6 +171,11 @@ contract UrbEAuction is ReentrancyGuard {
         emit ItemCanceled(msg.sender, nftAddress, tokenId);
     }
 
+    /**
+     * @notice Places a bid on an NFT auction
+     * @param nftAddress Address of the NFT contract
+     * @param tokenId ID of the NFT being auctioned
+     */
     function placeBid(
         address nftAddress,
         uint256 tokenId
@@ -170,6 +200,12 @@ contract UrbEAuction is ReentrancyGuard {
         emit HighestBidIncreased(msg.sender, nftAddress, tokenId, msg.value);
     }
 
+    /**
+     * @notice Ends an ongoing auction and transfers the NFT to the highest bidder.
+     * @param nftAddress Address of the NFT contract.
+     * @param tokenId Token ID of the NFT.
+     * @param auctionJson JSON representing the auction metadata.
+     */
     function auctionEnd(
         address nftAddress,
         uint256 tokenId,
@@ -177,15 +213,20 @@ contract UrbEAuction is ReentrancyGuard {
     ) external isListed(nftAddress, tokenId) nonReentrant {
         Listing memory listedItem = s_listings[nftAddress][tokenId];
 
+        // Verify that the auction has ended
         if (block.timestamp < listedItem.endTime) {
             revert AuctionNotYetEnded();
         }
 
+        // Determine the winner and transfer the NFT
         if (s_listings[nftAddress][tokenId].highestBidder == listedItem.seller) {
             delete (s_listings[nftAddress][tokenId]);
         } else {
+            // Transfer the sale price to the seller
             s_proceeds[listedItem.seller] += listedItem.price;
+            // Delete the listing
             delete (s_listings[nftAddress][tokenId]);
+            // Transfer the NFT to the winner
             IERC721(nftAddress).safeTransferFrom(
                 listedItem.seller,
                 listedItem.highestBidder,
@@ -193,6 +234,7 @@ contract UrbEAuction is ReentrancyGuard {
             );
         }
 
+        // Emit an event with details about the ended auction
         emit AuctionEnded(
             listedItem.highestBidder,
             nftAddress,
@@ -203,6 +245,9 @@ contract UrbEAuction is ReentrancyGuard {
         );
     }
 
+    /**
+     * @notice Allows the withdrawal of the accumulated proceeds for the given sender.
+     */
     function withdrawProceeds() external {
         uint256 proceeds = s_proceeds[msg.sender];
         if (proceeds <= 0) {
@@ -217,6 +262,12 @@ contract UrbEAuction is ReentrancyGuard {
     // Getter Functions //
     //////////////////////
 
+    /**
+     * @notice Getter function for a particular listing.
+     * @param nftAddress Address of the NFT contract.
+     * @param tokenId Token ID of the NFT.
+     * @return Returns the listing information for the specified NFT.
+     */
     function getListing(
         address nftAddress,
         uint256 tokenId
@@ -224,10 +275,19 @@ contract UrbEAuction is ReentrancyGuard {
         return s_listings[nftAddress][tokenId];
     }
 
+    /**
+     * @notice Getter function for the accumulated proceeds of a particular seller.
+     * @param seller Address of the seller.
+     * @return Returns the amount of accumulated proceeds for the specified seller.
+     */
     function getProceeds(address seller) external view returns (uint256) {
         return s_proceeds[seller];
     }
 
+    /**
+     * @notice Getter function for the deployer of the contract.
+     * @return Returns the address of the contract deployer.
+     */
     function getDeployer() public view returns (address) {
         return i_deployer;
     }
--- contracts/test/UrbEVehicleNft.sol
+++ contracts/test/UrbEVehicleNft.sol
@@ -2,20 +2,25 @@
 
 pragma solidity ^0.8.17;
 
+// Import the ERC721URIStorage contract from the OpenZeppelin library
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+// Create the UrbEVehicleNft contract, which inherits from ERC721URIStorage
 contract UrbEVehicleNft is ERC721URIStorage {
-    // NFT Variables
+    // Struct representing the data associated with an NFT
     struct NFT {
         string uri;
         string name;
     }
 
-    uint256 private s_tokenCounter;
-    uint256 private s_nftCount;
+    // Private state variables
+    uint256 private s_tokenCounter; // Tracks the total number of tokens minted
+    uint256 private s_nftCount; // Tracks the number of NFTs defined
 
+    // Mapping between an NFT index and its associated data
     mapping(uint256 => NFT) public s_nfts;
 
+    // Event emitted when an NFT is minted
     event NftMinted(uint256 indexed tokenId);
 
     constructor() ERC721("UrbE Vehicles NFT", "URBE") {
@@ -23,12 +28,23 @@ contract UrbEVehicleNft is ERC721URIStorage {
         s_nftCount = 0;
     }
 
-    // Main Functions
+    ////////////////////
+    // Main Functions //
+    ////////////////////
+    /**
+     * @notice Method for updating the mapping of NFT
+     * @param _uri URI for NFT
+     * @param _name Name for NFT
+     */
     function updateMappingNft(string memory _uri, string memory _name) public {
         s_nfts[s_nftCount] = NFT(_uri, _name);
         s_nftCount = s_nftCount + 1;
     }
 
+    /**
+     * @notice Method for minting an NFT
+     * @param index Index of NFT to mint
+     */
     function mintNft(uint256 index) public {
         uint256 newItemId = s_tokenCounter;
         s_tokenCounter = s_tokenCounter + 1;
@@ -37,7 +53,14 @@ contract UrbEVehicleNft is ERC721URIStorage {
         emit NftMinted(newItemId);
     }
 
-    // Getter Functions
+    //////////////////////
+    // Getter Functions //
+    //////////////////////
+
+    /**
+     * @notice Method for getting information about all NFTs in the mapping
+     * @return result Array of NFT structs containing URI and name for each NFT in the mapping
+     */
     function getNftInfos() public view returns (NFT[] memory) {
         NFT[] memory result = new NFT[](s_nftCount);
         for (uint256 i = 0; i < s_nftCount; i++) {
@@ -46,6 +69,10 @@ contract UrbEVehicleNft is ERC721URIStorage {
         return result;
     }
 
+    /**
+     * @notice Method for getting the current token counter value
+     * @return s_tokenCounter Current value of the token counter
+     */
     function getTokenCounter() public view returns (uint256) {
         return s_tokenCounter;
     }
